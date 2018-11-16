cf3b13c7fd2051f08dc8baa2df27e1506fffacd6
Matt Dean <matthewdean3d@gmail.com>
2018-11-15 23:05:56 -0800

    Duplicate visibility for testing

---
+++ Assets/Tests/Editor/VisibilityEditor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEditor;
+using kTools.Portals;
+using kTools.Portals.Tests;
+
+namespace kTools.PortalsEditor.Tests
+{
+	[CustomEditor(typeof(Visibility))]
+	public class VisibilityEditor : Editor
+	{
+		// -------------------------------------------------- //
+        //                    EDITOR STYLES                   //
+        // -------------------------------------------------- //
+
+        internal class Styles
+        {
+            public static GUIContent volumesSettingsText = EditorGUIUtility.TrTextContent("Volume Settings");
+			public static GUIContent modeText = EditorGUIUtility.TrTextContent("Mode");
+            public static GUIContent subdivisionsText = EditorGUIUtility.TrTextContent("Subdivisions");
+            public static GUIContent bakeSettingsText = EditorGUIUtility.TrTextContent("Bake Settings");
+            public static GUIContent rayDensityText = EditorGUIUtility.TrTextContent("Ray Count");
+            public static GUIContent coneAngleText = EditorGUIUtility.TrTextContent("Cone Angle");
+			public static GUIContent bakeToolsText = EditorGUIUtility.TrTextContent("Bake Tools");
+        }
+
+        // -------------------------------------------------- //
+        //                   PRIVATE FIELDS                   //
+        // -------------------------------------------------- //
+		
+		SerializedProperty m_VolumeModeProp;
+        SerializedProperty m_SubdivisionsProp;
+        SerializedProperty m_RayCountProp;
+        SerializedProperty m_ConeAngleProp;
+
+		// -------------------------------------------------- //
+        //                   PUBLIC METHODS                   //
+        // -------------------------------------------------- //
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            DrawVolumeSettings();
+            DrawBakeSettings();
+            
+			EditorGUILayout.LabelField(Styles.bakeToolsText, EditorStyles.boldLabel);
+            BakeEditor.DrawBakeTools(target as IBake);
+            serializedObject.ApplyModifiedProperties();
+        }
+		
+		// -------------------------------------------------- //
+        //                   PRIVATE METHODS                  //
+        // -------------------------------------------------- //
+
+        private void OnEnable()
+        {
+            m_VolumeModeProp = serializedObject.FindProperty("m_VolumeMode");
+            m_SubdivisionsProp = serializedObject.FindProperty("m_Subdivisions");
+            m_RayCountProp = serializedObject.FindProperty("m_RayCount");
+            m_ConeAngleProp = serializedObject.FindProperty("m_ConeAngle");
+        }
+
+        private void DrawVolumeSettings()
+        {
+            EditorGUILayout.LabelField(Styles.volumesSettingsText, EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(m_VolumeModeProp, Styles.modeText);
+            if(m_VolumeModeProp.enumValueIndex != (int)VolumeMode.Manual)
+                EditorGUILayout.PropertyField(m_SubdivisionsProp, Styles.subdivisionsText);
+            EditorGUILayout.Space();
+        }
+
+        private void DrawBakeSettings()
+        {
+            EditorGUILayout.LabelField(Styles.bakeSettingsText, EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(m_RayCountProp, Styles.rayDensityText);
+            EditorGUILayout.PropertyField(m_ConeAngleProp, Styles.coneAngleText);
+            EditorGUILayout.Space();
+        }
+	}
+}
---
+++ Assets/Tests/Runtime/Visibility.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using marijnz.EditorCoroutines;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+namespace kTools.Portals.Tests
+{
+	[ExecuteInEditMode]
+    [AddComponentMenu("kTools/Tests/Portals/Visibility")]
+	public class Visibility : MonoBehaviour, IBake 
+	{	
+		// -------------------------------------------------- //
+        //                   PRIVATE FIELDS                   //
+        // -------------------------------------------------- //
+
+		[SerializeField] private VolumeMode m_VolumeMode;
+		[SerializeField] private int m_Subdivisions;
+		[SerializeField] private int m_RayCount = 10;
+		[SerializeField] private float m_ConeAngle = 45.0f;
+		[SerializeField] private SerializablePortalData m_PortalData;
+
+		[SerializeField] private BakeState m_BakeState;
+		public BakeState bakeState
+		{
+			get { return m_BakeState; }
+		}
+
+		[SerializeField] private float m_Completion;
+		public float completion
+		{
+			get { return m_Completion; }
+		}
+
+        [SerializeField] private List<VisibilityDebug> m_Debugs;
+
+		private bool isInitialized = false;
+		private Dictionary<int, MeshRenderer> m_VisibleRenderers = new Dictionary<int, MeshRenderer>();
+		private List<PortalAgent> m_ActiveAgents = new List<PortalAgent>();
+
+#if UNITY_EDITOR
+		/// <summary>
+        /// Start the bake process. Editor only.
+        /// </summary>
+		public void OnClickBake()
+		{
+			EditorCoroutines.StartCoroutine(BakePortalData(), this);
+		}
+		
+		/// <summary>
+        /// Cancel any active bake and clear all data. Editor only.
+        /// </summary>
+		public void OnClickCancel()
+		{
+			// Reset all
+			EditorCoroutines.StopAllCoroutines(this);
+			m_BakeState = BakeState.Empty;	
+			m_PortalData = null;
+			m_Completion = 0;
+			UnityEditor.SceneView.RepaintAll();
+            UnityEditor.SceneManagement.EditorSceneManager.MarkAllScenesDirty();
+		}
+
+		private IEnumerator BakePortalData()
+		{
+			// Generate Portal data
+			m_Completion = 0.0f;
+			m_BakeState = BakeState.Occluders;
+			var occluders = PortalPrepareUtil.GetOccluderData();
+			m_BakeState = BakeState.Volumes;
+			var volumes = PortalPrepareUtil.GetVolumeData(m_VolumeMode, m_Subdivisions);
+			m_BakeState = BakeState.Visibility;
+			List<VisbilityData> visibilityTable = null;
+			yield return EditorCoroutines.StartCoroutine(GenerateVisibilityTable(occluders, volumes, value => visibilityTable = value), this);
+
+			// Serialize
+			m_PortalData = new SerializablePortalData()
+			{
+				occluders = occluders,
+				volumes = volumes,
+				visibilityTable = visibilityTable
+			};
+
+			// Finalize
+			m_BakeState = BakeState.Active;
+			m_Completion = 1.0f;
+			UnityEditor.SceneView.RepaintAll();
+            UnityEditor.SceneManagement.EditorSceneManager.MarkAllScenesDirty();
+		}
+
+		private IEnumerator GenerateVisibilityTable(SerializableOccluder[] occluders, SerializableVolume[] volumes, Action<List<VisbilityData>> result)
+		{
+			// Abort bake if input data is invalid
+			if(occluders == null || volumes == null)
+			{
+				Debug.LogError("Bake prepare data is invalid. Check bake output and retry.");
+				m_BakeState = BakeState.Empty;	
+				m_Completion = 0;
+				yield return null;
+			}
+
+			// Setup
+			m_BakeState = BakeState.Visibility;
+			var visibilityTable = new List<VisbilityData>();
+            m_Debugs = new List<VisibilityDebug>();
+
+			// Get Occluder proxies
+			var occluderProxies = PortalPrepareUtil.GetOccluderProxies(occluders);
+
+			// Get lowest subdivision volumes
+			var lowestSubdivisionVolumes = PortalPrepareUtil.FilterVolumeDataNoChildren(volumes);
+
+			// Get occludees
+			var occludees = PortalPrepareUtil.GetStaticOccludeeRenderers();
+
+			// Build Visibility for Volumes
+			for(int v = 0; v < lowestSubdivisionVolumes.Length; v++)
+			{
+				// Setup
+				m_Completion = (float)(v + 1) / (float)lowestSubdivisionVolumes.Length;
+				var volume = lowestSubdivisionVolumes[v];
+				var passedObjects = new List<GameObject>();
+
+				// Iterate random rays based on volume density
+				var rayCount = m_RayCount;
+				for(int r = 0; r < rayCount; r++)
+				{
+					// Get a random ray and a list of cone filtered renderers to test
+					var rayPosition = PortalVisibilityUtil.RandomPointWithinVolume(volume);
+					var rayDirection = PortalVisibilityUtil.RandomSphericalDistributionVector();
+					var filteredOccludees = PortalVisibilityUtil.FilterRenderersByConeAngle(occludees, rayPosition, rayDirection, m_ConeAngle);
+					for(int f = 0; f < filteredOccludees.Length; f++)
+					{
+						// Test ray against renderer AABB and occluders
+						if(PortalVisibilityUtil.CheckAABBIntersection(rayPosition, rayDirection, filteredOccludees[f].bounds))
+						{
+                            Vector3 hitPos;
+                            bool hit = CheckOcclusion(occluderProxies, filteredOccludees[f], rayPosition, rayDirection, out hitPos);
+							if(hit)
+								passedObjects.AddIfUnique(filteredOccludees[f].gameObject);
+
+                            m_Debugs.Add(new VisibilityDebug()
+                            {
+                                source = rayPosition,
+                                hit = hitPos,
+                                direction = rayDirection,
+                                isHit = hit,
+                            });
+						}
+					}
+				}
+
+				// Add to VisibilityTable
+				visibilityTable.Add(new VisbilityData(volume, passedObjects.ToArray()));
+				yield return null;
+			}
+
+			// Clear Occluder proxies
+			for(int i = 0; i < occluderProxies.Length; i++)
+				PortalCoreUtil.Destroy(occluderProxies[i].gameObject);
+
+			// Finalize
+			result(visibilityTable);
+		}
+
+        public static bool CheckOcclusion(Collider[] occluders, MeshRenderer occludee, Vector3 position, Vector3 direction, out Vector3 hitPos)
+		{
+            hitPos = Vector3.zero;
+
+			// If no occluders always return true
+			if(occluders == null || occluders.Length == 0)
+				return true;
+
+			// Raycast through all objects, return true if no collisions
+			var allHits = Physics.RaycastAll(position, direction);
+			if(allHits.Length == 0)
+				return true;
+			
+			// Get all occluders that intersected the ray, return true if no intersections
+			var intersectingOccluders = new Dictionary<Collider, Vector3>();
+			if(!TryGetIntersectingOccluders(occluders, allHits, out intersectingOccluders))
+				return true;
+			
+			// Filter by distance from ray source
+			intersectingOccluders.OrderBy(s => Vector3.Distance(position, s.Value));
+			var closestOccluder = intersectingOccluders.ElementAt(0);
+
+			// If it is the same object as the occludee always return true
+			if(closestOccluder.Key.gameObject == occludee.gameObject)
+				return true;
+
+            hitPos = closestOccluder.Value;
+
+			// TODO
+			// - Need intersection point with AABB
+			// Return true if the occluder is closer to the ray source than the occludee
+			return Vector3.Distance(position, closestOccluder.Value) > Vector3.Distance(position, occludee.bounds.center);
+		}
+
+        private static bool TryGetIntersectingOccluders(Collider[] occluders, RaycastHit[] hits, out Dictionary<Collider, Vector3> occluderHits)
+		{
+			// Iterate hits and occluders
+			// If occluders contains hit track the hit values
+			occluderHits = new Dictionary<Collider, Vector3>();
+			for(int h = 0; h < hits.Length; h++)
+			{
+				for(int o = 0; o < occluders.Length; o++)
+				{
+					if(hits[h].collider == occluders[o])
+						occluderHits.Add(occluders[o], hits[h].point);
+				}
+			}
+
+			// True if any occluders were hit
+			return occluderHits.Count > 0;
+		}
+
+        private void OnDrawGizmos()
+        {
+			if(m_BakeState != BakeState.Active || m_Debugs == null)
+				return;
+
+            foreach(VisibilityDebug debug in m_Debugs)
+            {
+                if(debug.isHit)
+                {
+                    PortalDebugUtil.DrawSphere(debug.source, 0.1f, PortalDebugColors.white);
+                    PortalDebugUtil.DrawSphere(debug.hit, 0.1f, PortalDebugColors.white);
+                    PortalDebugUtil.DrawLine(debug.source, debug.hit, PortalDebugColors.white);
+                }
+                else
+                {
+                    PortalDebugUtil.DrawSphere(debug.source, 0.1f, PortalDebugColors.black);
+                    PortalDebugUtil.DrawRay(debug.source, debug.direction, 10.0f, PortalDebugColors.black);
+                }
+            }
+
+			//foreach(SerializableOccluder occluder in m_PortalData.occluders)
+			//	PortalDebugUtil.DrawMesh(occluder.positionWS, occluder.rotationWS, occluder.scaleWS, 
+			//		occluder.mesh, PortalDebugColors.occluder);
+
+			foreach(SerializableVolume volume in PortalPrepareUtil.FilterVolumeDataNoChildren(m_PortalData.volumes))
+				PortalDebugUtil.DrawCube(volume.positionWS, volume.rotationWS, volume.scaleWS, PortalDebugColors.volume);
+        }
+
+        [Serializable]
+        public struct VisibilityDebug
+        {
+            public Vector3 source;
+            public Vector3 hit;
+            public Vector3 direction;
+            public bool isHit;
+        }
+#endif
+	}
+}
